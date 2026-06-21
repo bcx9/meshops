a631d2a1216b3aea32e45a2a79bc945ff18ca31b
bcx9 <33252755+bcx9@users.noreply.github.com>
2026-06-21 14:03:28 +0200

    Initialize database in Flask app

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from .config import Config
+from .database import db
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
 
     @app.get('/api/health')
     def health():
